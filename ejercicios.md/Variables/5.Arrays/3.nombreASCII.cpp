b8728c69343013092fc1f072e8663ddcbf7a14da
Fernando Silva <fernandosilv4c@gmail.com>
2017-01-12 20:32:25 +0100

    arrays 3/5 (Falta por subir ejercicio 2 , 4 y 6)

---
+++ ejercicios.md/Variables/5.Arrays/3.nombreASCII.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int nombre[] = {88,65,86,73,69,82,0};
+
+
+
+
+
+
+
+
+   
+
+    return EXIT_SUCCESS;
+}
---
+++ ejercicios.md/Variables/5.Arrays/5.imprimir_char.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+char palabra [] = "holaaaaaaaaaaaaaa";
+
+	printf("%s",palabra);
+
+
+
+
+
+
+   
+
+    return EXIT_SUCCESS;
+}
