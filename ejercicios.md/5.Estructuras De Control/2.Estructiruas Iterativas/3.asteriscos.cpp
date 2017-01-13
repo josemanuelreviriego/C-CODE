55408f03d4d4da58bd4fa0f8a92993bcb9d6d85a
Fernando Silva <fernandosilv4c@gmail.com>
2017-01-14 00:41:56 +0100

    1/6 Quedan 5 por hacer

---
+++ ejercicios.md/5.Estructuras De Control/2.Estructiruas Iterativas/3.asteriscos.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int asteriscos;
+
+	printf("Asteriscos = ");
+	scanf(" %i",&asteriscos);
+	for(int i=0; i<asteriscos; i++){
+	printf("*");
+	}
+
+
+
+
+
+
+   
+
+    return EXIT_SUCCESS;
+}
