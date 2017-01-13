7e7fe433222b7055fc9dd3160fb5b07c082e4335
Fernando Silva <fernandosilv4c@gmail.com>
2017-01-13 19:13:03 +0100

    1/7 ejercicio 2 terminado

---
+++ ejercicios.md/3.Operadores/2.A nivel de Bits/2.hexa.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int numero;
+
+	printf("Numero ");
+	scanf(" %i",&numero);
+
+	printf("%i = %x",numero,numero);
+
+
+
+
+   
+
+    return EXIT_SUCCESS;
+}
