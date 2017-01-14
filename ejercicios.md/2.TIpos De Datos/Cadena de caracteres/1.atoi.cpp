e52c42299165ab64b4dedbd779c0baf61874b804
Fernando Silva <fernandosilv4c@gmail.com>
2017-01-15 00:04:35 +0100

    ejercicio atoi (Falta usar sscanf)

--- ejercicios.md/2.TIpos De Datos/Cadena de caracteres/1.atoi.cpp
+++ ejercicios.md/2.TIpos De Datos/Cadena de caracteres/1.atoi.cpp
@@ -6,10 +6,8 @@ int main(){
 char dni[9];
 int dni_entero;
 
-	printf("Numero DNI= ");
-	scanf(dni" %s",dni);
-	
-	printf("Letra DNI= ");
+	printf("DNI= ");
+	scanf(" %s",dni);
 	dni_entero=atoi(dni);
 
 	printf("%i",dni_entero);
