fc00d1c3c5c29b9de02b57d1ac0a32261d372453
Fernando Silva <fernandosilv4c@gmail.com>
2017-01-14 16:29:49 +0100

    2/6

---
+++ ejercicios.md/5.Estructuras De Control/2.Estructiruas Iterativas/1.repetidamente.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int numero;
+
+do{
+	printf("Dime un numero del 1-10");
+	scanf(" %i",&numero);
+	}
+	while(numero<1 || numero>10);
+
+
+
+
+
+
+
+   
+
+    return EXIT_SUCCESS;
+}
