57448b7f9e257624e1bc3c43b3a9752f6cad5773
Fernando Silva <fernandosilv4c@gmail.com>
2017-03-03 18:10:52 +0100

    Probando reglas

---
+++ Moi/10_Makefile/2_probando_reglas/calculadora.c
@@ -0,0 +1,3 @@
+#include "funciones.h"
+
+int sumar(int a,int b){return a+b;}
---
+++ Moi/10_Makefile/2_probando_reglas/main.c
@@ -0,0 +1,8 @@
+#include "funciones.h"
+
+int main(int argc,char **argv){
+	int suma = sumar(2,2);
+	presentar(suma);
+	return 0;
+
+}
