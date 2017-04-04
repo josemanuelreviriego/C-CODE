abc372027befbebd93c97ad14148dbedb2630510
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-04 22:41:16 +0200

    Ordenando

---
+++ Actualmente_Programando/Prueba/include/skel.h
@@ -0,0 +1,20 @@
+#ifndef __******_H__
+#define __******_H__
+
+	//Liberias y Estructuras
+
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+	//Funciones
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
