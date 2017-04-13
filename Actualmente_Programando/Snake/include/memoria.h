17a924dcbcc6e5345576426be70452bef02c2afc
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-13 20:25:37 +0200

    Creacion dinamica del cuerpo de la serpiente

---
+++ Actualmente_Programando/Snake/include/memoria.h
@@ -0,0 +1,24 @@
+#ifndef __MEMORIA_H__
+#define __MEMORIA_H__
+
+	//Liberias y Estructuras
+
+#include "main.h"
+
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+	//Funciones
+
+	CoorSerp *NuevoCuerpo();
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Actualmente_Programando/Snake/src/memoria.cpp
@@ -0,0 +1,15 @@
+#include "memoria.h"
+
+//Reserva dinamicamente los nuevos cuerpos
+
+CoorSerp *NuevoCuerpo(){
+
+	CoorSerp *nuevos;
+
+	nuevos = (CoorSerp *)malloc(sizeof(CoorSerp));
+	
+
+
+		
+	return nuevos;
+}
