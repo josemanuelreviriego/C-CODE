7881751fc2cca29a9b47c70ac59276d71907c557
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-14 16:03:25 +0200

    Ahora no se mueve en direcciones inversas

--- Actualmente_Programando/Snake/include/serpiente.h
+++ Actualmente_Programando/Snake/include/serpiente.h
@@ -16,8 +16,8 @@ extern "C"{
 	void cabeza_serpiente(CuerpoSerpiente *serpiente,CoorSerp *Cuerpo,int y,int x);
 	bool push(CuerpoSerpiente *serpiente,CoorSerp *NuevoCuerpo);
 	void jugador_mueve(CuerpoSerpiente *serpiente,Direccion *serpiente_jugador);
-
-
+	void mostrar(CuerpoSerpiente *serpiente_pantalla);
+	void movimiento_auto(CuerpoSerpiente *serpiente_auto,Direccion *direccion_usuario);
 
 #ifdef __cplusplus
 }
