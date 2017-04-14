df2be870acaaf02a48d263562810ab01207c409d
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-14 16:02:30 +0200

    Ahora la serpiente se mueve solo en una direccion

--- Actualmente_Programando/Snake/src/serpiente.cpp
+++ Actualmente_Programando/Snake/src/serpiente.cpp
@@ -15,34 +15,83 @@ void jugador_mueve(CuerpoSerpiente *serpiente,Direccion *serpiente_jugador){
         switch(serpiente_jugador->ordenes = getch() ){
 	
 		case KEY_UP:
-			serpiente->cuerpo[serpiente->cabeza]->coor_y +=-1;
-			serpiente_jugador->avanzar.coor_y = -1;
-			serpiente_jugador->avanzar.coor_x = 0;
+
+			if(serpiente_jugador->direccion_actual != ABAJO){
+
+				serpiente->cuerpo[serpiente->cabeza]->coor_y +=-1;
+				serpiente_jugador->avanzar.coor_y = -1;
+				serpiente_jugador->avanzar.coor_x = 0;
+				serpiente_jugador->direccion_actual = ARRIBA;
+
+			}
+
 			break;
+
+			
 
 		case KEY_RIGHT:
-			serpiente->cuerpo[serpiente->cabeza]->coor_x +=1;
-			serpiente_jugador->avanzar.coor_y = 0;
-			serpiente_jugador->avanzar.coor_x = 1;
+
+			if(serpiente_jugador->direccion_actual != IZQUIERDA){
+
+				serpiente->cuerpo[serpiente->cabeza]->coor_x +=1;
+				serpiente_jugador->avanzar.coor_y = 0;
+				serpiente_jugador->avanzar.coor_x = 1;
+				serpiente_jugador->direccion_actual = DERECHA;
+
+			}
+
 			break;
 
+			
+
 		case KEY_LEFT:
-			serpiente->cuerpo[serpiente->cabeza]->coor_x +=-1;
-			serpiente_jugador->avanzar.coor_y = 0;
-			serpiente_jugador->avanzar.coor_x = -1;
+
+			if(serpiente_jugador->direccion_actual != DERECHA){
+
+				serpiente->cuerpo[serpiente->cabeza]->coor_x +=-1;
+				serpiente_jugador->avanzar.coor_y = 0;
+				serpiente_jugador->avanzar.coor_x = -1;
+				serpiente_jugador->direccion_actual = IZQUIERDA;
+
+			}
+
 			break;
 
 		case KEY_DOWN:
-			serpiente->cuerpo[serpiente->cabeza]->coor_y +=1;
-			serpiente_jugador->avanzar.coor_y = 1;
-			serpiente_jugador->avanzar.coor_x = 0;
+
+			if(serpiente_jugador->direccion_actual != ARRIBA){
+
+				serpiente->cuerpo[serpiente->cabeza]->coor_y +=1;
+				serpiente_jugador->avanzar.coor_y = 1;
+				serpiente_jugador->avanzar.coor_x = 0;
+				serpiente_jugador->direccion_actual = ABAJO;
+
+			}
+
 			break;
+
+
 	
 	}
 
 }
 
+void mostrar(CuerpoSerpiente *serpiente_pantalla){
+        
+        for(int i=0; i<serpiente_pantalla->cima; i++)
+                mvprintw(serpiente_pantalla->cuerpo[i]->coor_y,serpiente_pantalla->cuerpo[i]->coor_x,"o");
 
+}
+
+void movimiento_auto(CuerpoSerpiente *serpiente_auto,Direccion *direccion_usuario){
+
+        for(int i=0; i<serpiente_auto->cima; i++){
+	
+	                serpiente_auto->cuerpo[i]->coor_x += direccion_usuario->avanzar.coor_x;
+	                serpiente_auto->cuerpo[i]->coor_y += direccion_usuario->avanzar.coor_y;
+	
+	        } 
+}
 
 bool push(CuerpoSerpiente *serpiente,CoorSerp *NuevoCuerpo){
 
