57488967f1d01d6fdeb9cac579540cba76b6c056
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-15 21:16:16 +0200

    Ordenando

---
+++ Skel/src/skel.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct prueba{
+
+	int a;
+
+}Prueba;
+
+struct Prueba *nombre(){
+
+	struct Prueba *name;
+
+
+	return name;
+
+}
+
+
+int main (){
+	
+	struct (*a)(void);
+	a = nombre;
+
+	
+
+
+	return EXIT_SUCCESS;
+
+}
