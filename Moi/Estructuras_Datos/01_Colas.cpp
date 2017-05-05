6829bb7ecaef55f52c4f4ee817293cd8ef7036f4
Fernando Silva <fernandosilv4c@gmail.com>
2017-05-06 01:46:03 +0200

    Insertar elementos en la cola

---
+++ Moi/Estructuras_Datos/01_Colas.cpp
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Nodo{
+
+	int numero;
+	Nodo *siguiente;
+	
+}Nodo;
+
+bool cola_vacia(Nodo **inicio){return !*inicio;};
+
+void insertar(Nodo **inicio,Nodo **fin,int n){
+
+	Nodo *nuevo_nodo = (Nodo *)malloc(sizeof(Nodo));
+		nuevo_nodo->numero = n;
+		nuevo_nodo->siguiente = NULL;
+
+	if(cola_vacia(inicio))
+		*inicio = nuevo_nodo;
+
+	else
+		(*fin)->siguiente = nuevo_nodo;
+
+
+	*fin = nuevo_nodo;
+
+}
+
+int main(){
+
+	Nodo *inicio = NULL;
+	Nodo *fin = NULL;
+	
+	insertar(&inicio,&fin,5);
+	insertar(&inicio,&fin,6);
+	insertar(&inicio,&fin,7);
+
+	while(inicio!=NULL){
+
+		printf("%i ",inicio->numero);
+		inicio = inicio->siguiente;
+
+
+
+	}
+
+	return EXIT_SUCCESS;
+}
+
