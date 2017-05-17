0432a4a9c0226966fb3e8ee67c0dfde63f177070
Fernando Silva <fernandosilv4c@gmail.com>
2017-05-17 21:39:15 +0200

    Librerias

---
+++ Actualmente_Programando/Tetris/include/graficos.h
@@ -0,0 +1,26 @@
+#ifndef __GRAFICOS_H__
+#define __GRAFICOS_H__
+
+#include <ncurses.h>
+#include "general.h"
+
+
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+	//Funciones
+
+
+	void iniciar(Pantalla *a);
+
+	void terminar();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Actualmente_Programando/Tetris/include/tablero.h
@@ -0,0 +1,22 @@
+#ifndef __TABLERO_H__
+#define __TABLERO_H__
+
+#include "general.h"
+#include "graficos.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+	//Funciones
+
+
+		void tablero(Pantalla juego,Pantalla puntuacion);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
