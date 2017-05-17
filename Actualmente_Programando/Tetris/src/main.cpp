4d599d2275fb90979b8f63ecdcf667228577d3b4
Fernando Silva <fernandosilv4c@gmail.com>
2017-05-17 21:37:06 +0200

    Creado interfaz para la puntuacion

--- Actualmente_Programando/Tetris/src/main.cpp
+++ Actualmente_Programando/Tetris/src/main.cpp
@@ -1,58 +1,17 @@
 #include "general.h"
 #include "graficos.h"
-
-#define A 60
-#define B 10
-#define C 20
-#define D 10
-
-void tam_tablero(Pantalla *coor_pantalla){
-
-		coor_pantalla->fin.x = coor_pantalla->max.x-A;
-		coor_pantalla->fin.y = coor_pantalla->max.y-B;
-		coor_pantalla->principio.x = C;
-		coor_pantalla->principio.y = D;
-
-}
-
-
-void tablero(Pantalla coor_pantalla){
-
-		tam_tablero(&coor_pantalla);
-
-		//Muro inferior		
-		for(int i=0; i<coor_pantalla.fin.x-coor_pantalla.principio.x; i++)
-				mvaddch(coor_pantalla.fin.y,coor_pantalla.fin.x-i,ACS_HLINE);
-				mvaddch(coor_pantalla.fin.y,coor_pantalla.fin.x,ACS_LRCORNER);
-				mvaddch(coor_pantalla.principio.y,coor_pantalla.principio.x,ACS_ULCORNER);
-
-		//Muro superior
-		for(int i=0; i<coor_pantalla.fin.x-coor_pantalla.principio.x; i++)
-				mvaddch(coor_pantalla.principio.y,coor_pantalla.fin.x-i,ACS_HLINE);
-				mvaddch(coor_pantalla.principio.y,coor_pantalla.fin.x,ACS_URCORNER);
-				mvaddch(coor_pantalla.fin.y,coor_pantalla.principio.x,ACS_LLCORNER);
-
-
-		//Muro izquierda
-		for(int i=1;i<coor_pantalla.fin.y-coor_pantalla.principio.y; i++)
-				mvaddch(coor_pantalla.principio.y+i,coor_pantalla.principio.x,ACS_VLINE);
-				
-		//Muro derecha
-		for(int i=1;i<coor_pantalla.fin.y-coor_pantalla.principio.y; i++)
-				mvaddch(coor_pantalla.principio.y+i,coor_pantalla.fin.x,ACS_VLINE);
-
-		refresh();
-}
+#include "tablero.h"
 
 int main(int argc, char *argv[]){
 	
-		Pantalla coor_pantalla;
+		Pantalla pantalla_juego;
+		Pantalla pantalla_puntuacion;
 
-		iniciar(&coor_pantalla);	
+		iniciar(&pantalla_juego);	
 
 		do{
 
-				tablero(coor_pantalla);
+				tablero(pantalla_juego,pantalla_puntuacion);
 
 		}while(true);
 		
---
+++ Actualmente_Programando/Tetris/src/tablero.cpp
@@ -0,0 +1,71 @@
+#include "tablero.h"
+
+#define DERECHA_JUEGO 70
+#define ABAJO_JUEGO 5
+#define IZQUIERDA_JUEGO 5
+#define ARRIBA_JUEGO 5
+
+#define DERECHA_PUNTUACION 5
+#define ABAJO_PUNTUACION 5
+#define IZQUIERDA_PUNTUACION 150
+#define ARRIBA_PUNTUACION 5
+
+void tam_tablero_juego(Pantalla *juego){
+
+	juego->fin.x = juego->max.x-DERECHA_JUEGO;
+	juego->fin.y = juego->max.y-ABAJO_JUEGO;
+	juego->principio.x = IZQUIERDA_JUEGO;
+	juego->principio.y = ARRIBA_JUEGO;
+
+}
+
+void tam_tablero_puntuacion(Pantalla *puntuacion){
+
+	puntuacion->fin.x = puntuacion->max.x-DERECHA_PUNTUACION;
+	puntuacion->fin.y = puntuacion->max.y-ABAJO_PUNTUACION;
+	puntuacion->principio.x = IZQUIERDA_PUNTUACION;
+	puntuacion->principio.y = ARRIBA_PUNTUACION;
+
+
+}
+
+void dibuja_tablero(Pantalla juego){
+
+		//Muro inferior		
+		for(int i=0; i<juego.fin.x-juego.principio.x; i++)
+				mvaddch(juego.fin.y,juego.fin.x-i,ACS_HLINE);
+				mvaddch(juego.fin.y,juego.fin.x,ACS_LRCORNER);
+				mvaddch(juego.principio.y,juego.principio.x,ACS_ULCORNER);
+
+		//Muro superior
+		for(int i=0; i<juego.fin.x-juego.principio.x; i++)
+				mvaddch(juego.principio.y,juego.fin.x-i,ACS_HLINE);
+				mvaddch(juego.principio.y,juego.fin.x,ACS_URCORNER);
+				mvaddch(juego.fin.y,juego.principio.x,ACS_LLCORNER);
+
+		//Muro izquierda
+		for(int i=1;i<juego.fin.y-juego.principio.y; i++)
+				mvaddch(juego.principio.y+i,juego.principio.x,ACS_VLINE);
+				
+		//Muro derecha
+		for(int i=1;i<juego.fin.y-juego.principio.y; i++)
+				mvaddch(juego.principio.y+i,juego.fin.x,ACS_VLINE);
+
+		refresh();
+
+}
+
+void tablero(Pantalla juego,Pantalla puntuacion){
+
+		puntuacion.max.x = juego.max.x;
+		puntuacion.max.y = juego.max.y;
+
+		tam_tablero_juego(&juego);
+		dibuja_tablero(juego);
+		
+		tam_tablero_puntuacion(&puntuacion);
+		dibuja_tablero(puntuacion);
+
+
+
+}
