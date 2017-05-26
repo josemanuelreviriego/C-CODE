756414a841ef4cb5558674a3f06a2ee555146825
Fernando Silva <fernandosilv4c@gmail.com>
2017-05-26 20:50:42 +0200

    Se añaden colores

--- Actualmente_Programando/Tetris/include/graficos.h
+++ Actualmente_Programando/Tetris/include/graficos.h
@@ -4,6 +4,14 @@
 #include <ncurses.h>
 #include "general.h"
 
+#define GRIS 0
+#define FONDO 1
+#define ROJO 2
+#define VERDE 3
+#define AMARILLO 4
+#define AZUL 5
+#define MORADO 6
+#define AQUA 7	
 
 
 
