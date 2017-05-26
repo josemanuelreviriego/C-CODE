12df991cab204f04a6233d11b01a56b7aa7f24ef
Fernando Silva <fernandosilv4c@gmail.com>
2017-05-27 00:02:48 +0200

    Se crean funciones para generar el movimiento de las piezas

--- Actualmente_Programando/Tetris/src/main.cpp
+++ Actualmente_Programando/Tetris/src/main.cpp
@@ -7,15 +7,37 @@
 
 #define SUELO 300
 
-void pintar_figura(Pila *pila){
+void pintar_figura(Pila pila){
 
-		while(pila->tetris[0]){
-				attron(COLOR_PAIR(pila->tetris[0]->color));
-				mvaddch(pila->tetris[0]->coor.y,pila->tetris[0]->coor.x,ACS_CKBOARD);
-				attroff(COLOR_PAIR(pila->tetris[0]->color));
-				pila->tetris[0] = pila->tetris[0]->sig_estructura;
+		while(pila.tetris[0]){
+
+				attron(COLOR_PAIR(pila.tetris[0]->color));
+				mvaddch(pila.tetris[0]->coor.y,pila.tetris[0]->coor.x,ACS_CKBOARD);
+				attroff(COLOR_PAIR(pila.tetris[0]->color));
+				pila.tetris[0] = pila.tetris[0]->sig_estructura;
 		}
-	
+
+					refresh();
+}
+
+void borrar_figura(Pila pila){
+
+		while(pila.tetris[0]){
+
+				attron(COLOR_PAIR(pila.tetris[0]->color));
+				mvaddch(pila.tetris[0]->coor.y,pila.tetris[0]->coor.x,' ');
+				attroff(COLOR_PAIR(pila.tetris[0]->color));
+				pila.tetris[0] = pila.tetris[0]->sig_estructura;
+		}
+
+}
+
+void mover_figura(Pila pila){
+
+		while(pila.tetris[0]){
+			pila.tetris[0]->coor.y+=0.00001;
+			pila.tetris[0] = pila.tetris[0]->sig_estructura;
+		}
 }
 
 int main(int argc, char *argv[]){
@@ -23,22 +45,30 @@ int main(int argc, char *argv[]){
 		Pila pila;
 			pila.cima = 0;
 
-		while(pila.cima < MAX_PIEZAS){push(&pila,construir_figura());};
+	//while(pila.cima < MAX_PIEZAS){push(&pila,construir_figura());};
 
 		Pantalla pantalla;
 		iniciar(&pantalla);	
+
+		int p;
 
 		do{
 
 				tablero(&pantalla);
 				rellenar(pantalla);
-				//if(pila.cima == 0 || pila.tetris[pila.cima]->coor.x == SUELO)
-						//generar_figura(&pila); funcion para crear la primera figura y meterla en la pila, en lugar de usar el bucle de arriba
-						
-				pintar_figura(&pila);
-				//mover_figura(&pila); funcion para poder ir moviendo la figura que se encuentre en la pila
 
-				refresh();
+				do{
+
+					if(pila.cima == 0 || pila.tetris[pila.cima]->coor.x == SUELO)
+						push(&pila,construir_figura());
+					
+				mover_figura(pila);
+				pintar_figura(pila);
+				borrar_figura(pila);
+
+				}while(1);
+
+
 				
 		}while(true);
 
