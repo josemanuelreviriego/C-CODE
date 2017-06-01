f580ead7de8dfc94c8b4ae3d3ed4feb5c48e47c9
Fernando Silva <fernandosilv4c@gmail.com>
2017-06-01 21:29:44 +0200

    Pruebas con void y return

---
+++ Pruebas/10_return.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void imprime(){
+	
+	printf("Hola");
+
+	return;
+
+	printf("Adios");
+
+
+}
+
+
+int main(){
+
+	imprime();
+
+	return EXIT_SUCCESS;
+}
+
+
